a8816e99094364a338881878bc923f97c87f2840
╚»«╝ El'XiaoYinDeHioa ╚»«╝ <budierik457@gmail.com>
2025-01-13 19:25:46 +0800

    Add files via upload

---
+++ pb_helios.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from helios device
+$(call inherit-product, device/infinix/helios/device.mk)
+
+# Inherit some common PBRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := helios
+PRODUCT_NAME := pb_helios
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := HOT 20S
+PRODUCT_MANUFACTURER := infinix
